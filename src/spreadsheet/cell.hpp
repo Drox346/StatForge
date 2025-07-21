6e68e2bc03b70cf41f69ccc098a2a7f669acb8c0
Drox346 <78570563+Drox346@users.noreply.github.com>
2025-07-21 20:59:21 +0200

    adding cell type member to cell

--- src/spreadsheet/cell.hpp
+++ src/spreadsheet/cell.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "common/definitions.hpp"
+#include "common/internal/definitions.hpp"
 
 #include <functional>
 
@@ -8,10 +8,17 @@ namespace statforge {
 
 using CellFormula = std::function<CellValue()>;
 
+enum class CellType : u_int8_t {
+    Value,
+    Formula,
+    Aggregator,
+};
+
 struct Cell {
     CellFormula formula;
     bool dirty{false};
     CellValue value{};
+    CellType type;
 };
 
 } // namespace statforge
